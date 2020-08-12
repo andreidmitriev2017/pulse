10761c0a0cc3954f1e1cd7bdaea3bcbf3b4eab37
Andrei Dmitriev <andreid.dmitrieff2017@mail.ru>
2020-08-12 19:08:56 +0500

    Fixed validation errors

--- src/js/script.js
+++ src/js/script.js
@@ -106,7 +106,7 @@ $(document).ready(function(){
             $('.page-up').fadeOut();
         };
     });
-    $("a[href^='#']").click(function(){
+    $("a[href=#up]").click(function(){
             const _href = $(this).attr("href");
             $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
             return false;
